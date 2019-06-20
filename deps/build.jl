78006284e2993f66b199c836a0d3bf902c54fac2
Avi Kulkarni <avinash@mis.mpg.de>
2019-06-20 21:02:23 +0200

    Changed build again.

--- deps/build.jl
+++ deps/build.jl
@@ -1,16 +1,3 @@
-# Add the Dory dependency by hand.
-#
-# This workaround is a grevious misuse of the package manager. Unfortunately, the
-# Julia team haven't mentioned what the correct way to do this is.
-# I do not want to keep track of multiple repositories...
-import Pkg
 
-println("Building...")
-
-# Search the installed package list for Dory. If it isn't there, install it.
-try Pkg.installed()["Dory"]
-catch
-    Pkg.activate("$(@__DIR__)/..")
-    Pkg.develop(Pkg.PackageSpec(path="$(@__DIR__)/../Dory"))
-    Pkg.resolve()
-end
+# Update the load path so Julia can find Dory inside the repo.
+push!(LOAD_PATH, "$(@__DIR__)/../Dory/src/")
