94f8bf5ef0e0df7808133cc69690c961a233b181
Christian Eder <ederc@mathematik.uni-kl.de>
2019-08-01 09:32:52 +0200

    changes order to DRL, commented finite field computation since char 0 computation takes too long

--- test/lgr-sing.jl
+++ test/lgr-sing.jl
@@ -9,10 +9,11 @@ end
 
 R, (x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33, x34, x35, x36, x37, x38, x39, x40) =
     PolynomialRing(Singular.QQ,
+    #PolynomialRing(Singular.N_ZpField(32003),
                    ["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12",
                     "x13", "x14", "x15", "x16", "x17", "x18", "x19", "x20", "x21", "x22", "x23",
                     "x24", "x25", "x26", "x27", "x28", "x29", "x30", "x31", "x32", "x33", "x34",
-                    "x35", "x36", "x37", "x38", "x39", "x40"], ordering=:deglex)
+                    "x35", "x36", "x37", "x38", "x39", "x40"], ordering=:degrevlex)
 
 ps = 
 "x7*x23-x10*x29+x11*x30",
