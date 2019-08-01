08e466d311d6fcdb2c8839a3d9c3b4fd348ef2f4
Christian Eder <ederc@mathematik.uni-kl.de>
2019-08-01 09:31:59 +0200

    adds linear equations to lgr example, uses slimgb for computing a GB

--- test/lgr-sing.jl
+++ test/lgr-sing.jl
@@ -1,3 +1,12 @@
+using Singular
+
+function random_linear_equations(R::PolyRing)
+    var_vec   = Vector(gens(R))
+    rand_vec  = Vector([rand(-1000:1000) for i in 1:length(gens(R))])
+
+    return (transpose(var_vec)*rand_vec)
+end
+
 R, (x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33, x34, x35, x36, x37, x38, x39, x40) =
     PolynomialRing(Singular.QQ,
                    ["x1", "x2", "x3", "x4", "x5", "x6", "x7", "x8", "x9", "x10", "x11", "x12",
@@ -234,5 +243,6 @@ ps =
 "x1^2+x25*x27-x26*x34"
 
 id = Singular.Ideal(R, [eval(Meta.parse("$p")) for p in ps])
-
-@time G = Singular.std(id);
+idl = Singular.Ideal(R, [random_linear_equations(R) for i = 1:11])
+@time id = id + idl
+@time G = Singular.slimgb(id);
