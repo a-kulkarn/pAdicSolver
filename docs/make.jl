70ff128438fcd50e09853e319e206f0b63f8fd09
a-kulkarn <avinash@mis.mpg.de>
2019-09-30 22:11:04 +0200

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -3,7 +3,7 @@ push!(LOAD_PATH,"../src/")
 using Pkg, Documenter, DocumenterMarkdown, Dory, pAdicSolver
 makedocs(sitename="pAdicSolver.jl Documentation")
 
-# deploydocs(
-#   repo = "github.com/a-kulkarn/Dory.git",
-# )
+deploydocs(
+  repo = "github.com/a-kulkarn/pAdicSolver.git",
+)
 
