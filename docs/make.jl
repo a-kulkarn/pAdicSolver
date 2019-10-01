e6c15800f090e06e5bc970d494c1daf702291def
Avi Kulkarni <avinash@mis.mpg.de>
2019-10-01 13:18:48 +0200

    Updated deploydocs to change devbranch.

--- docs/make.jl
+++ docs/make.jl
@@ -4,6 +4,6 @@ using Pkg, Documenter, DocumenterMarkdown, Dory, pAdicSolver
 makedocs(sitename="pAdicSolver.jl Documentation")
 
 deploydocs(
-  repo = "github.com/a-kulkarn/pAdicSolver.git",
+  repo = "github.com/a-kulkarn/pAdicSolver.git", devbranch="padicSolvers"
 )
 
