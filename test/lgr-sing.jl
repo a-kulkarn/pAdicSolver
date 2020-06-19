5cf0b944cd62de8c8fa29b9b78d65f6b9f80ef10
Avi Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2020-06-19 05:54:10 -0400

    Minor lgr test changes.

--- test/lgr-sing.jl
+++ test/lgr-sing.jl
@@ -15,9 +15,10 @@ R, Rvars =
                     "x24", "x25", "x26", "x27", "x28", "x29", "x30", "x31", "x32", "x33", "x34",
                     "x35", "x36", "x37", "x38", "x39", "x40"], ordering=:degrevlex)
 
+(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33, x34, x35, x36, x37, x38, x39, x40) = Rvars
+
 include(".lgr_eqns.jl")
 
-(x1, x2, x3, x4, x5, x6, x7, x8, x9, x10, x11, x12, x13, x14, x15, x16, x17, x18, x19, x20, x21, x22, x23, x24, x25, x26, x27, x28, x29, x30, x31, x32, x33, x34, x35, x36, x37, x38, x39, x40) = Rvars
 
 sid   = Singular.Ideal(R, [eval(Meta.parse("$p")) for p in ps])
 sidl  = Singular.Ideal(R, [random_linear_equations(R) for i = 1:11])
