8cc81c53265be8617e814cc2d03791a26662c712
Avi Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2022-01-30 18:37:28 -0500

    Added files.

---
+++ src/singular_dependancy.jl
@@ -0,0 +1,62 @@
+
+import Hecke.ishomogeneous
+function ishomogeneous(I::Singular.sideal)
+    G = Singular.gens(I)
+    return all(ishomogeneous, G)
+end
+
+function ishomogeneous(f::Singular.spoly)
+    d = total_degree(first(monomials(f)))
+    return all(m->total_degree(m)==d, monomials(f))
+end
+
+
+function truncated_normal_form(I::Singular.sideal{<:Singular.spoly{<:T}} where T)
+    return Singular.std(I)
+end
+
+"""
+Returns the affine solutions to the polynomial equations defined by I.
+The system must be 0 dimensional.
+"""
+function padic_solutions(I::Singular.sideal{<:Singular.spoly{<:T}} where T, R;
+                         eigenvector_method="power")
+
+    # Presently, the user is responsible for providing the padic ring R.
+    #
+    # Question: Is it possible to decide the right precision for the user at this stage?
+
+    base_ring = Singular.base_ring(I)
+    X  = gens(base_ring)
+    GB = truncated_normal_form(I)
+    ish = ishomogeneous(GB)
+
+    ## Construct the multiplication matrices directly.
+    B = Singular.gens(Singular.kbase(GB))
+
+    xi_operators = []
+    for v in vcat([base_ring(1)], X)
+        push!(xi_operators, [Singular.reduce(v*b, GB) for b in B] )
+    end
+
+    
+    M = [matrix(R, [[coeff(g, b) for b in B] for g in op]) for op in xi_operators]    
+    M = [m.entries for m in M]
+
+    # @info "" M[1] M[2] M[3]
+    
+    t0 = time()
+    Xi = normalized_simultaneous_eigenvalues(M, ish, eigenvector_method)
+    
+    # In the affine system, the distinguished monomial (i.e, "1" for that case) does 
+    # not correspond to a coordinate.
+    if ish return Xi else return  Xi[:,2:size(Xi,2)] end
+end
+
+
+# Function that should be implemented in Oscar
+function (R::Hecke.FlintRationalField)(a::Singular.n_Z)
+    n = convert(BigInt, a)
+    return R(n)
+end
+
---
+++ test/low-precision-handling.jl
@@ -0,0 +1,22 @@
+
+
+@testset "Precision handling" begin
+    p = 1000003
+    N = 3
+    K = Hecke.PadicField(p, N)
+    R, (x1, x2, x3, x4, x5) = Hecke.PolynomialRing(K, 5)
+    
+    GB = [2*x3 - 39000117*x4 + 5*x5,
+          1000003*x2 - 1,
+          x1 - 1,
+          21996131716197184*x4*x5 + 35100208458309461*x4 - 3158009474*x5^2 - 5232015125*x5 - 985,
+          4182039634666882990820525906472*x4^2 + 1597064285781592634326475*x4 - 92060053845332998*x5^2 - 241723414460067851*x5 - 56121168503,
+          (16324044970945243992612460993471986029743497621*x4 + 1635912215634320703145614369145738988672*x5^3 + 3145181581802732307984319762588946014782*x5^2
+          - 799304976455501742146405393837628704021*x5 + 4579907243568677265846766872644839)]
+
+    #pAdicSolver.solve_affine_system(GB, eigenvector_method=:tropical)
+    
+    @test_throws Dory.InsufficientPrecisionError pAdicSolver.solve_affine_system(GB, eigenvector_method=:tropical)
+
+    @test true
+end
