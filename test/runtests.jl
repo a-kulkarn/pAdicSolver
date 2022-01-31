dae03f76cf5d5e40d73cb58fced928697d854f94
Avi Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2022-01-30 22:10:51 -0500

    Included tests again.

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,8 +1,8 @@
 
 using Test, Hecke, Dory, pAdicSolver
 
-#include("basic_plane_test.jl")
+include("basic_plane_test.jl")
 include("Seibenundzwanzig.jl")
-#include("projective_tests.jl")
-#include("weird_system.jl")
-#include("low-precision-handling.jl")
+include("projective_tests.jl")
+include("weird_system.jl")
+include("low-precision-handling.jl")
