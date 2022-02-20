e7d121cfd21143b85b5960281d43527c2e66ce4e
Avinash Kulkarni <avinash.a.kulkarni@dartmouth.edu>
2022-02-20 01:29:24 -0500

    Renamed test file.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,7 +2,7 @@
 using Test, Hecke, Dory, pAdicSolver
 
 @testset verbose = true "pAdicSolver" begin
-    include("basic_plane_test.jl")
+    include("Interface_test.jl")
     include("Seibenundzwanzig.jl")
     include("random_system_test.jl")
     include("weird_system.jl")
